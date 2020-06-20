5de5d6d27626f5298137b9611a67fbdb76dc97c9
unknown <inu.sohoni@gmail.com>
2020-06-20 19:59:50 +0530

    added additional pages of services and contact

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -6,4 +6,11 @@ def index(request):
 
 def about(request):
     return HttpResponse("This is page is About Us")
+
+def services(request):
+    return HttpResponse("These are our services") 
+
+def contact(request):
+    return HttpResponse("This is our Contact Page") 
     
+          
