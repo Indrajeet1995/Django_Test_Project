2c5f6e1192ae259f25f314fd51d3264c6d336902
unknown <inu.sohoni@gmail.com>
2020-06-26 06:43:01 +0530

    Added Template blocks

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -9,12 +9,14 @@ def index(request):
     return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse("This is page is About Us")
+    return render(request, 'about.html')
+    # return HttpResponse("This is page is About Us")
 
 def services(request):
-    return HttpResponse("These are our services") 
+    # return HttpResponse("These are our services")
+    return render(request, 'services.html') 
 
 def contact(request):
-    return HttpResponse("This is our Contact Page") 
-    
+    # return HttpResponse("This is our Contact Page") 
+    return render(request, 'contact.html')
           
