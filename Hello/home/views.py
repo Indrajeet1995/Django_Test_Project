f9c579d18f0ade60d70e3d3d2b5083a1ab822380
unknown <inu.sohoni@gmail.com>
2020-06-28 06:54:52 +0530

    Backend ready Contact Us Page

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
+ 
 # Create your views here.
 def index(request):
     # context = {
@@ -18,5 +22,13 @@ def services(request):
 
 def contact(request):
     # return HttpResponse("This is our Contact Page") 
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Thanks for Contacting Us!')
     return render(request, 'contact.html')
           
